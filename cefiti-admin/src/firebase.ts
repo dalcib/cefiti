0385cb67c63d86b726987d63f2f70f0de9349932
dalcib <dalcib@gmail.com>
2026-04-10 15:44:43 +0800

    gitignore

--- cefiti-admin/src/firebase.ts
+++ cefiti-admin/src/firebase.ts
@@ -3,15 +3,8 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-  projectId: 'cefiti',
-  appId: '1:485822995260:web:5a03fca2431e8976c1dc39',
-  storageBucket: 'cefiti.firebasestorage.app',
-  apiKey: 'AIzaSyBIKMel_XS1J54NL3nPquKs0rw5PNKD-Rs',
-  authDomain: 'cefiti.firebaseapp.com',
-  messagingSenderId: '485822995260',
-  measurementId: 'G-TN0N3V94ZZ',
-}
+import firebaseConfig from '../firebase-config.json'
+
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
